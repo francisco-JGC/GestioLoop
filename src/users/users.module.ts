78023dd8848c351f408fc8cd188fb421aa5e3243
francisco-JGC <jjjchico1@gmail.com>
2025-02-16 01:10:30 -0600

    feat: enhance Users module with ExternalUser entity and service integration

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,15 @@
-import { Module, forwardRef } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
-import { User } from './entities/user.entity';
+import { Module } from '@nestjs/common';
+import { UsersService } from './services/users.service';
+import { UsersController } from './controllers/users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { RolesModule } from 'src/roles/roles.module';
+import { ExternalUser } from './entities/external-user.entity';
+import { User } from './entities/user.entity';
+import { ExternalUsersService } from './services/external-user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => RolesModule)],
+  imports: [TypeOrmModule.forFeature([User, ExternalUser])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, ExternalUsersService],
+  exports: [UsersService, ExternalUsersService],
 })
 export class UsersModule {}
