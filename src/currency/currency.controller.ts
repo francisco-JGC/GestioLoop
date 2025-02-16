8562bc71ad414341ad112a80d075ddb65005a363
Francisco-Contollo <francisco.garcia@contollo.net>
2025-02-15 22:15:43 -0600

    feat: implement Currency module with controller, service, and DTOs

---
+++ src/currency/currency.controller.ts
@@ -0,0 +1,17 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { CurrencyService } from './currency.service';
+import { CreateCurrencyDto } from './dto/create-currency.dto';
+import { UpdateCurrencyDto } from './dto/update-currency.dto';
+
+@Controller('currency')
+export class CurrencyController {
+  constructor(private readonly currencyService: CurrencyService) {}
+}
---
+++ src/currency/currency.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCurrencyDto } from './dto/create-currency.dto';
+import { UpdateCurrencyDto } from './dto/update-currency.dto';
+
+@Injectable()
+export class CurrencyService {}
