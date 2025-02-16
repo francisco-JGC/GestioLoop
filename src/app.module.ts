ab335548a8a610c3791c93c732df2fda3d56e1cd
Francisco-Contollo <francisco.garcia@contollo.net>
2025-02-15 21:43:48 -0600

    feat: add Tenant, Currency, and Branch modules to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { RolesModule } from './roles/roles.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getDatabaseConfig } from './_config/database.config';
+import { TenantModule } from './tenant/tenant.module';
+import { CurrencyModule } from './currency/currency.module';
+import { BranchModule } from './branch/branch.module';
 
 @Module({
   imports: [
@@ -23,7 +25,9 @@ import { getDatabaseConfig } from './_config/database.config';
 
     AuthModule,
     UsersModule,
-    RolesModule,
+    TenantModule,
+    CurrencyModule,
+    BranchModule,
   ],
   controllers: [AppController],
   providers: [AppService],
