4935e92d77de7de099f87c02055e5ae1f7d86bdc
francisco-JGC <jjjchico1@gmail.com>
2025-02-23 17:07:10 -0600

    feat: integrate TypeOrmModule and JwtAuthGuard into BranchModule

--- src/branch/branch.module.ts
+++ src/branch/branch.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { BranchService } from './branch.service';
 import { BranchController } from './branch.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Branch } from './entities/branch.entity';
+import { TenantModule } from 'src/tenant/tenant.module';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Branch]), TenantModule],
   controllers: [BranchController],
-  providers: [BranchService],
+  providers: [BranchService, JwtAuthGuard],
+  exports: [BranchService],
 })
 export class BranchModule {}
