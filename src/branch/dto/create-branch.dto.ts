65e55f0e947383792c7a1f1048f931c587600f14
francisco-JGC <jjjchico1@gmail.com>
2025-02-23 17:06:35 -0600

    feat: add validation and Swagger documentation to CreateBranchDto

--- src/branch/dto/create-branch.dto.ts
+++ src/branch/dto/create-branch.dto.ts
@@ -1,1 +1,14 @@
-export class CreateBranchDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateBranchDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+}
