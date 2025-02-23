02145c2044c3f83867cbfd935928c87f88664337
francisco-JGC <jjjchico1@gmail.com>
2025-02-23 17:06:18 -0600

    feat: add Swagger API tags to TenantController and export TenantService in TenantModule

--- src/tenant/controllers/tenant.controller.ts
+++ src/tenant/controllers/tenant.controller.ts
@@ -4,7 +4,9 @@ import { Roles } from 'src/auth/decorators/roles.decorator';
 import { UserRole } from 'src/_shared/constants/user-types.enums';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('tenant')
 @Controller('tenant')
 export class TenantController {
   constructor(private readonly tenantService: TenantService) {}
--- src/tenant/tenant.module.ts
+++ src/tenant/tenant.module.ts
@@ -16,5 +16,6 @@ import { UsersModule } from 'src/users/users.module';
   ],
   controllers: [TenantController],
   providers: [TenantService],
+  exports: [TenantService],
 })
 export class TenantModule {}
