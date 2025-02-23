4c0be32a55a8497884f29dd357458de974d956d6
francisco-JGC <jjjchico1@gmail.com>
2025-02-23 17:07:20 -0600

    feat: implement configureBranch method in BranchService for branch creation with tenant validation

--- src/branch/branch.service.ts
+++ src/branch/branch.service.ts
@@ -1,6 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { CreateBranchDto } from './dto/create-branch.dto';
-import { UpdateBranchDto } from './dto/update-branch.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Branch } from './entities/branch.entity';
+import { DataSource, Repository } from 'typeorm';
+import { TenantService } from 'src/tenant/services/tenant.service';
+import { HttpResponse } from 'src/_shared/HttpResponse';
 
 @Injectable()
-export class BranchService {}
+export class BranchService {
+  constructor(
+    @InjectRepository(Branch)
+    private readonly branchRepo: Repository<Branch>,
+
+    private tenantService: TenantService,
+    private dataSoruce: DataSource,
+  ) {}
+
+  async configureBranch(
+    tenantId: string,
+    createBranchDto: CreateBranchDto,
+  ): Promise<HttpResponse> {
+    const tenant = await this.tenantService.getTenantById(tenantId);
+
+    if (!tenant) {
+      return {
+        statusCode: HttpStatus.NOT_FOUND,
+        message: 'Tenant not found',
+      };
+    }
+
+    const existingBranch = tenant.branches.find(
+      (branch) => branch.name === createBranchDto.name,
+    );
+
+    if (existingBranch) {
+      return {
+        statusCode: HttpStatus.CONFLICT,
+        message: 'There is already a branch with this name',
+      };
+    }
+
+    const newBranch = this.branchRepo.create(createBranchDto);
+
+    await this.dataSoruce.transaction(async (manager) => {
+      await manager.save(newBranch);
+
+      tenant.branches.push(newBranch);
+
+      await manager.save(tenant);
+    });
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Branch created',
+      data: newBranch,
+    };
+  }
+}
