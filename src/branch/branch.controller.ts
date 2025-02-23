0f1a6e7b39c978458370b983f5e79e44c7ea47af
francisco-JGC <jjjchico1@gmail.com>
2025-02-23 17:07:14 -0600

    feat: add configureBranch endpoint with JWT and role-based guards in BranchController

--- src/branch/branch.controller.ts
+++ src/branch/branch.controller.ts
@@ -1,15 +1,20 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { BranchService } from './branch.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { UserRole } from 'src/_shared/constants/user-types.enums';
+import { ApiTags } from '@nestjs/swagger';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
+@ApiTags('branch')
 @Controller('branch')
 export class BranchController {
   constructor(private readonly branchService: BranchService) {}
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.SUPER_ADMIN)
+  @Post('configure-branch')
+  async configureBranch(@Req() req) {
+    return this.branchService.configureBranch(req.user.tenantId, req.body);
+  }
 }
