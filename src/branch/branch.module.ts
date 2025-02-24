e925d9142f14ae36a4269f355e9f16152e4c14c4
francisco-JGC <jjjchico1@gmail.com>
2025-02-23 22:33:50 -0600

    feat: add UsersModule to Branch module imports for user management integration

--- src/branch/branch.module.ts
+++ src/branch/branch.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Branch } from './entities/branch.entity';
 import { TenantModule } from 'src/tenant/tenant.module';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Branch]), TenantModule],
+  imports: [TypeOrmModule.forFeature([Branch]), TenantModule, UsersModule],
   controllers: [BranchController],
   providers: [BranchService, JwtAuthGuard],
   exports: [BranchService],
